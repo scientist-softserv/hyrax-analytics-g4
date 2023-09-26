c0c6f241a413aba35d1177ea470a308f5d788029
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-09-26 09:41:45 -0400

    🚧 Add pagination handling

--- lib/hyrax/analytics/g4.rb
+++ lib/hyrax/analytics/g4.rb
@@ -17,36 +17,47 @@ module Hyrax
         end
       end
 
-      def self.call
+      def self.call(limit: 25_000, offset: 0)
         client = ::Google::Analytics::Data::V1beta::AnalyticsData::Client.new
-        # For details on the schema: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema
-        # For examples in the wild: https://github.com/JasonBarnabe/greasyfork/blob/master/lib/google_analytics.rb
-        request = ::Google::Analytics::Data::V1beta::RunReportRequest.new(
-          # This is the roll-up identifier as indicated in the UI.  You need to prefix this, I think,
-          # with the "properties/" string.
-          property: "properties/395286330",
-          date_ranges: [{ start_date: "2023-09-22", end_date: "2023-09-24" }],
-          # Note: The order of dimensions and metrics matter
-          dimensions: [{ name: 'date' }, { name: 'eventName' }, { name: 'pagePath' }, { name: 'hostName' }],
-          metrics: [{ name: 'eventCount' }],
-          ##
-          # See https://developers.google.com/analytics/devguides/reporting/data/v1/advanced#weekly_cohorts_and_using_cohorts_with_other_api_features
-          dimension_filter: { filter: { field_name: 'eventName', in_list_filter: { values: ["work-view", "file-set-download"] } } },
-          limit: 25_000,
-          offset: 0
-        )
+        returning_value = []
 
-        results = client.run_report request
-
-        results.rows.map do |result|
-          Result.new(
-            date: Date.parse(result.dimension_values[0].value),
-            event_name: result.dimension_values[1].value,
-            page_location: result.dimension_values[2].value,
-            host_name: result.dimension_values[3].value,
-            event_count: result.metric_values[0].value.to_i
+        loop do
+          # For details on the schema: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema
+          # For examples in the wild: https://github.com/JasonBarnabe/greasyfork/blob/master/lib/google_analytics.rb
+          request = ::Google::Analytics::Data::V1beta::RunReportRequest.new(
+            # This is the roll-up identifier as indicated in the UI.  You need to prefix this, I think,
+            # with the "properties/" string.
+            property: "properties/395286330",
+            date_ranges: [{ start_date: "2023-09-22", end_date: "2023-09-24" }],
+            # Note: The order of dimensions and metrics matter
+            dimensions: [{ name: 'date' }, { name: 'eventName' }, { name: 'pagePath' }, { name: 'hostName' }],
+            metrics: [{ name: 'eventCount' }],
+            ##
+            # See https://developers.google.com/analytics/devguides/reporting/data/v1/advanced#weekly_cohorts_and_using_cohorts_with_other_api_features
+            dimension_filter: { filter: { field_name: 'eventName', in_list_filter: { values: ["work-view", "file-set-download"] } } },
+            limit: limit,
+            offset: offset
           )
+          results = client.run_report request
+          break if results.rows.empty?
+
+          this_results = results.rows.map do |result|
+            Result.new(
+              date: Date.parse(result.dimension_values[0].value),
+              event_name: result.dimension_values[1].value,
+              page_location: result.dimension_values[2].value,
+              host_name: result.dimension_values[3].value,
+              event_count: result.metric_values[0].value.to_i
+            )
+          end
+
+          returning_value += this_results
+          # No sense making another request if got less than the limit.
+          break if this_results.size < limit
+          offset += limit
         end
+
+        returning_value
       end
     end
   end

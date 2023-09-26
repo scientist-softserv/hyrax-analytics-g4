0d5fe4362d776650835546d3e219bb4b06b7db34
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-09-26 09:18:16 -0400

    🚧 Refining query to reflect Hyrax

--- lib/hyrax/analytics/g4.rb
+++ lib/hyrax/analytics/g4.rb
@@ -10,6 +10,8 @@ module Hyrax
 
       require "google/analytics/data/v1beta"
 
+      Row = Struct.new(:date, :event_name, :page_location, :event_count, keyword_init: true)
+
       def self.call
         client = ::Google::Analytics::Data::V1beta::AnalyticsData::Client.new
         # For details on the schema: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema
@@ -18,12 +20,28 @@ module Hyrax
           # This is the roll-up identifier as indicated in the UI.  You need to prefix this, I think,
           # with the "properties/" string.
           property: "properties/395286330",
-          date_ranges: [{ start_date: "2023-08-01", end_date: "2023-09-24" }],
-          dimensions: [{ name: 'date' }, { name: 'pagePath' }],
-          metrics: [{ name: 'screenPageViews' }]
+          date_ranges: [{ start_date: "2023-09-22", end_date: "2023-09-24" }],
+
+          # Note: The order of dimensions and metrics matter
+          dimensions: [{ name: 'date' }, { name: 'eventName' }, { name: 'pageLocation' }],
+          metrics: [{ name: 'eventCount' }],
+          ##
+          # See https://developers.google.com/analytics/devguides/reporting/data/v1/advanced#weekly_cohorts_and_using_cohorts_with_other_api_features
+          dimension_filter: { filter: { field_name: 'eventName', in_list_filter: { values: ["work-view", "file-set-download"] } } },
+          limit: 25_000,
+          offset: 0
         )
 
-        response = client.run_report request
+        results = client.run_report request
+
+        results.rows.map do |result|
+          Row.new(
+            date: Date.parse(result.dimension_values[0].value),
+            event_name: result.dimension_values[1].value,
+            page_location: result.dimension_values[2].value,
+            event_count: result.metric_values[0].value.to_i
+          )
+        end
       end
     end
   end

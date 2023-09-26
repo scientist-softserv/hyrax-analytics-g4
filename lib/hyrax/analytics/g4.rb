13afc28b537cf6139f902f3b732f9fa69dcc8430
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-09-26 09:33:49 -0400

    🚧 Add ID and more documentation for results

--- lib/hyrax/analytics/g4.rb
+++ lib/hyrax/analytics/g4.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
 require_relative "g4/version"
+require "google/analytics/data/v1beta"
 
 module Hyrax
   module Analytics
     module G4
       class Error < StandardError; end
-      # Your code goes here...
 
-      require "google/analytics/data/v1beta"
-
-      Row = Struct.new(:date, :event_name, :page_location, :event_count, keyword_init: true)
+      ##
+      # Creating a Struct so we can see what the data is instead of relying on the positional nature
+      # of the returned results.
+      Result = Struct.new(:date, :event_name, :page_location, :host_name, :event_count, keyword_init: true) do
+        def id
+          page_location.split('/').last
+        end
+      end
 
       def self.call
         client = ::Google::Analytics::Data::V1beta::AnalyticsData::Client.new
@@ -21,9 +26,8 @@ module Hyrax
           # with the "properties/" string.
           property: "properties/395286330",
           date_ranges: [{ start_date: "2023-09-22", end_date: "2023-09-24" }],
-
           # Note: The order of dimensions and metrics matter
-          dimensions: [{ name: 'date' }, { name: 'eventName' }, { name: 'pageLocation' }],
+          dimensions: [{ name: 'date' }, { name: 'eventName' }, { name: 'pagePath' }, { name: 'hostName' }],
           metrics: [{ name: 'eventCount' }],
           ##
           # See https://developers.google.com/analytics/devguides/reporting/data/v1/advanced#weekly_cohorts_and_using_cohorts_with_other_api_features
@@ -35,10 +39,11 @@ module Hyrax
         results = client.run_report request
 
         results.rows.map do |result|
-          Row.new(
+          Result.new(
             date: Date.parse(result.dimension_values[0].value),
             event_name: result.dimension_values[1].value,
             page_location: result.dimension_values[2].value,
+            host_name: result.dimension_values[3].value,
             event_count: result.metric_values[0].value.to_i
           )
         end
